373a843a96bd9dd55ec2db8cfb4c7b60eabdba66
liujingxing <18268800083@163.com>
2019-05-28 14:18:08 +0800

    增加P层案例

--- app/src/main/java/com/example/rxlife/Presenter.java
+++ app/src/main/java/com/example/rxlife/Presenter.java
@@ -1,32 +1,46 @@
 package com.example.rxlife;
 
-import android.arch.lifecycle.LifecycleOwner;
-import android.util.Log;
-
 import com.rxjava.rxlife.RxLife;
 
 import java.util.concurrent.TimeUnit;
 
+import androidx.lifecycle.GenericLifecycleObserver;
+import androidx.lifecycle.Lifecycle.Event;
+import androidx.lifecycle.LifecycleOwner;
 import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
 
 /**
  * User: ljx
  * Date: 2019-05-26
  * Time: 15:20
  */
-public class Presenter {
+public class Presenter implements GenericLifecycleObserver {
 
     private LifecycleOwner mOwner;
 
     public Presenter(LifecycleOwner owner) {
-        mOwner = owner;
+        owner.getLifecycle().addObserver(this);
     }
 
-    public void observable() {
+    @Override
+    public void onStateChanged(LifecycleOwner source, Event event) {
+        //Activity 生命周期回调
+        mOwner = source;
+        if (event == Event.ON_DESTROY) {  //Activity销毁
+            mOwner.getLifecycle().addObserver(this);
+        }
+    }
+
+    private void test() {
         Observable.intervalRange(1, 100, 0, 200, TimeUnit.MILLISECONDS)
             .as(RxLife.asOnMain(mOwner))
-            .subscribe(aLong -> {
-                Log.e("LJX", "accept=" + aLong + " Thread=" + Thread.currentThread());
+            .subscribe(new Consumer<Long>() {
+                @Override
+                public void accept(Long aLong) throws Exception {
+
+                }
             });
+
     }
 }
