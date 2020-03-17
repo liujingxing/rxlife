f73d9a9d1ebbc3407b35f84dce4b7d1196e32c89
liujingxing <18268800083@163.com>
2020-03-17 22:48:33 +0800

    RxLifeScope 异常捕获机制修改

--- rxlife-x/src/main/java/com/rxjava/rxlife/RxLifeScope.kt
+++ rxlife-x/src/main/java/com/rxjava/rxlife/RxLifeScope.kt
@@ -5,14 +5,18 @@ import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
 import kotlinx.coroutines.*
 import java.io.Closeable
-import kotlin.coroutines.CoroutineContext
 
 /**
+ * 此类用于开启协程，并自动捕获异常
+ *
+ * 在FragmentActivity、 ViewModel环境下，使用 [rxLifeScope.launch] 方式开启协程，会在页面销毁时，自动关闭协程  (注意：这里的rxLifeScope是变量，不是类名)
+ *
+ * 其它环境下，需要拿到 [RxLifeScope.launch]方法的返回值后，手动调用[Job.cancel]方法关闭协程
  * User: ljx
  * Date: 2020-03-14
  * Time: 10:30
  */
-open class RxLifeScope() : Closeable {
+class RxLifeScope() : Closeable {
 
     constructor(
         owner: LifecycleOwner,
@@ -33,31 +37,39 @@ open class RxLifeScope() : Closeable {
         })
     }
 
-    //协程异常回调
-    private val exceptionHandler = CoroutineExceptionHandler { coroutine, throwable ->
-        catch(coroutine, throwable)
-    }
-
-    private var onError: ((Throwable) -> Unit)? = null
-
     private val coroutineScope: CoroutineScope =
-        CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate + exceptionHandler)
+        CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
 
     fun launch(block: suspend CoroutineScope.() -> Unit): Job {
         return launch(block, null)
     }
 
+    /**
+     * @param block     协程代码块，运行在UI线程
+     * @param onError   异常回调，运行在UI线程
+     * @param onStart   协程开始回调，运行在UI线程
+     * @param onFinally 协程结束回调，不管成功/失败，都会回调，运行在UI线程
+     */
     fun launch(
         block: suspend CoroutineScope.() -> Unit,
-        error: ((Throwable) -> Unit)? = null
+        onError: ((Throwable) -> Unit)? = null,
+        onStart: (() -> Unit)? = null,
+        onFinally: (() -> Unit)? = null
     ): Job {
-        onError = error
-        return coroutineScope.launch { block() }
-    }
-
-    //出现异常时执行
-    private fun catch(coroutine: CoroutineContext, e: Throwable) {
-        onError?.invoke(e)
+        return coroutineScope.launch {
+            try {
+                onStart?.invoke()
+                block()
+            } catch (e: Throwable) {
+                if (onError != null) {
+                    onError(e)
+                } else {
+                    e.printStackTrace()
+                }
+            } finally {
+                onFinally?.invoke()
+            }
+        }
     }
 
     override fun close() {
